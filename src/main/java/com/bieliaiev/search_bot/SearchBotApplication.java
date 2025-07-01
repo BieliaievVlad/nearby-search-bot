4df229ef6cdfcd94b1c849e2d88a4081002301ed
BieliaievVlad <rostik.khokhlov@gmail.com>
2025-07-01 15:13:58 +0300

    Initial Commit

---
+++ src/main/java/com/bieliaiev/search_bot/SearchBotApplication.java
@@ -0,0 +1,13 @@
+package com.bieliaiev.search_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bieliaiev/sushibot/SearchBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bieliaiev.sushibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushibotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
